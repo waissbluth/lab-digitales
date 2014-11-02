6ea51a78b4d9db1b16e6329b37766fdcb014d7b1
Nicolas Waissbluth <nwaissbluth@gmail.com>
2014-11-01 23:21:32 -0300

    Bleh.
    Former-commit-id: 9c92862c8aaf4262a2a0a31b43fe8921f3e16c61

--- Experiencia_5/fuseRelaunch.cmd
+++ Experiencia_5/fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "Z:/Desktop/LabDigitales/Experiencia_5/jump_tf_isim_beh.exe" -prj "Z:/Desktop/LabDigitales/Experiencia_5/jump_tf_beh.prj" "work.jump_tf" "work.glbl" 
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "Z:/Desktop/LabDigitales/Experiencia_5/sound_tf_isim_beh.exe" -prj "Z:/Desktop/LabDigitales/Experiencia_5/sound_tf_beh.prj" "work.sound_tf" "work.glbl" 
---
+++ Experiencia_5/sound.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:52:51 11/01/2014 
+// Design Name: 
+// Module Name:    sound 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module sound(
+	input clk,
+	input [4:0] note,
+	output [8:0] value
+    );
+	 
+	reg [8:0] count;
+	reg [26:0] remainder_accum;
+	
+	wire [5:0] jump;
+	wire [26:0] remainder;
+	
+	jump jumper(note, jump, remainder);
+	
+	sin_wave siner(clk, count, value);
+	
+	initial begin
+		count = 0;
+		remainder_accum = 0;
+	end
+	
+	always @(posedge clk) begin
+		count <= count + jump;
+		remainder_accum <= remainder_accum + remainder;
+	
+	end
+
+endmodule
