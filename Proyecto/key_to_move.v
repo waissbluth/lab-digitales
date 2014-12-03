4d0e0dc6004207d65c916d29339a7562b1b60ab0
Nicolas Waissbluth <nwaissbluth@gmail.com>
2014-12-03 19:38:10 -0300

    Keys rules?
    Former-commit-id: 8afff373052b75d9f01a2b78dde8e29487bddddd

--- Proyecto/key_to_move.v
+++ Proyecto/key_to_move.v
@@ -32,14 +32,14 @@ module key_to_move(
 	 always @(posedge clk) begin
 		if(newKey) begin
 			case(keyCode)
-				8'b01110100: begin
-					next_move <= right;
-				end 8'b01110010: begin
-					next_move <= down;
-				end 8'b01110101: begin
-					next_move <= up;
-				end 8'b01101011: begin
-					next_move <= left;
+				8'b01110100: begin //To Right
+					if(move != left) next_move <= right;
+				end 8'b01110010: begin // To Down
+					if(move != up) next_move <= down;
+				end 8'b01110101: begin // To Up
+					if(move != down) next_move <= up;
+				end 8'b01101011: begin // To Left
+					if(move != right) next_move <= left;
 				end 
 			endcase	
 			
