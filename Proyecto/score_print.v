ba109ce04a9262a836ec4ace3e8f2e1c1095b85d
Nicolas Waissbluth <nwaissbluth@gmail.com>
2014-12-03 20:23:00 -0300

    Fontsy.
    Former-commit-id: 819209ffe8c8c94c273c6bd430563edda84dee07

---
+++ Proyecto/score_print.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:14:52 12/03/2014 
+// Design Name: 
+// Module Name:    score_print 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module score_print #(
+		parameter H = 32,
+		parameter V = 32
+)(		input clk,
+		
+		input evalX,
+		input evalY,
+		input posX,
+		input posY,
+		output reg col
+	
+    );
+	 
+	function integer logb2;
+		input integer n;
+		begin
+			n = n-1;
+		for(logb2=0; n>0; logb2=logb2 + 1)
+			n = n>>1;
+		end
+	endfunction
+
+
+endmodule
